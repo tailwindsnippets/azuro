63fc4b582a64d591edbafe551ac5ecb660da239b
Walu G. Gondwe <gowalumbe@gmail.com>
2025-03-22 16:29:36 +0200

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,26 +1,25 @@
-import cx from 'classnames'
+import cx from 'classnames';
 
-import { Href } from 'components/navigation'
-
+import { Href } from 'components/navigation';
 
 type LogoProps = {
-  className?: string
-}
+  className?: string;
+};
 
 const Logo: React.FC<LogoProps> = (props) => {
-  const { className } = props
+  const { className } = props;
 
   return (
-    <Href to="/" className={cx('flex items-center', className)}> 
-<img
-  className="h-full"
-  src="/images/logo.png"
-  alt="Smart contracts png"
-  width="71"
-  height="16"
-/>
+    <Href to="/" className={cx('flex items-center', className)}>
+      <img
+        className="h-full"
+        src="/images/logo.png"
+        alt="Smart contracts png"
+        width="71"
+        height="16"
+      />
     </Href>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
