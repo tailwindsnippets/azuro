fcea928eeba07afa26356887f1be3346915f8026
Walu G. Gondwe <gowalumbe@gmail.com>
2025-03-25 10:38:47 +0200

    added logo imp

--- src/compositions/PageLayout/components/Content/components/LeftSidebar/LeftSidebar.tsx
+++ src/compositions/PageLayout/components/Content/components/LeftSidebar/LeftSidebar.tsx
@@ -10,12 +10,12 @@ import LiveSwitcher from 'compositions/LiveSwitcher/LiveSwitcher'
 const LeftSidebar: React.FC = () => {
   return (
     <div className="h-full">
-      <div className="px-4 py-5 sticky top-0 flex items-center justify-between">
-        <Logo className="h-6" />
-        {/* <button className="text-grey-60 hover:text-grey-90 transition" onClick={() => openModal('SearchModal')}>
-          <Icon className="size-5" name="interface/search" />
-        </button> */}
-      </div>
+<div className="px-4 py-5 sticky top-0 flex items-center justify-between">
+  <Logo className="h-10 w-10" /> {/* Adjust h-10 and w-10 to your preferred size */}
+  {/* <button className="text-grey-60 hover:text-grey-90 transition" onClick={() => openModal('SearchModal')}>
+    <Icon className="size-5" name="interface/search" />
+  </button> */}
+</div>
       <div className="overflow-auto wd:h-[calc(100vh_-_4rem)] no-scrollbar">
         <LiveSwitcher />
         <Navigation className="mt-2" />
