c74ee70977ca5daeb38c34a746964039d1c6e760
Walu G. Gondwe <gowalumbe@gmail.com>
2025-03-25 07:04:38 +0200

    added svg logo

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,6 +1,6 @@
-import cx from 'classnames';
-
-import { Href } from 'components/navigation';
+import cx from "classnames";
+import Image from "next/image";
+import { Href } from "components/navigation";
 
 type LogoProps = {
   className?: string;
@@ -10,13 +10,15 @@ const Logo: React.FC<LogoProps> = (props) => {
   const { className } = props;
 
   return (
-    <Href to="/" className={cx('flex items-center', className)}>
-      <img
+    <Href to="/" className={cx("flex items-center", className)}>
+      <Image
         className="h-full"
-        src="/images/logo.png"
-        alt="Smart contracts png"
-        width="71"
-        height="16"
+        src="/images/logo.svg"
+        alt="Smart contracts bets"
+        width={71}
+        height={16}
+        // Optional: Disable optimization if SVG has issues
+        unoptimized={true}
       />
     </Href>
   );
